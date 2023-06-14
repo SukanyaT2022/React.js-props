abbd3a3ca3294c023db8606722bd3b1cff100b38
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-06-14 10:04:41 -0400

    add

--- src/component/ChangeText.js
+++ src/component/ChangeText.js
@@ -1,11 +1,27 @@
 import React, { useState } from 'react'
-
 function ChangeText() {
 
 const[text, setText] = useState('old friend')
 const ChangeText =()=>{
-    setText('New friends')
+    setText(text=="old friend"? "new friend": "old friend")  // one way this line change new friend and go back to old friend
 }
+// second way way this line change new friend and go back to old friend
+//one way use if else
+// const ChangeText =()=>{
+//   setText('New friend')
+//     if(text === "old friend"){
+//       setText ("New friend")
+//     }else{
+//       setText ("old friend")
+//     }
+// }
+
+//if not use if else we use 
+// (x>10)?console.log():console.log()
+
+
+
+
 
   return (
     <div>
